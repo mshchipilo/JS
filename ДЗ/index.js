444dc94ae79f30eb9035d750bd42f52087893594
maria_shchipilo <schipilo1994@mail.ru>
2021-03-30 20:25:27 +0300

    first commit

---
+++ ДЗ/index.js
@@ -0,0 +1,35 @@
+// 1 задание
+let str11=('Привет')
+console.log(typeof str11)
+
+let num12=123
+console.log(typeof num12)
+
+let flag=true
+console.log(typeof flag)
+
+// 2 задание 
+let a1=5+3
+console.log(a1)
+
+let a2=5-3
+console.log(a2)
+
+let a3=5*3
+console.log(a3)
+
+let a4=5/3
+console.log(a4)
+
+// 3 Задание
+let a6=5
+let resultA6=a6%3
+console.log(resultA6)
+
+let a7=3
+let resultA7=a7%5
+console.log(resultA7)
+
+let a10='кг'
+let resultA10=`75 ${a10}`
+console.log(resultA10)
