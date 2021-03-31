532470afd370ff9988034671f66bdc3cf12f2520
maria_shchipilo <schipilo1994@mail.ru>
2021-03-31 17:56:21 +0300

    added 2

--- ДЗ/index.js
+++ ДЗ/index.js
@@ -8,6 +8,9 @@ console.log(typeof num12)
 let flag=true
 console.log(typeof flag)
 
+let txt="true"
+console.log(typeof txt)
+
 // 2 задание 
 let a1=5+3
 console.log(a1)
@@ -26,6 +29,14 @@ let a6=5
 let resultA6=a6%3
 console.log(resultA6)
 
+// let a7=5
+// let resultA7=a7+'3'
+// console.log(resultA7)  
+
+let a8='5'
+let resultA8=a8-3
+console.log(resultA8)
+
 let a7=3
 let resultA7=a7%5
 console.log(resultA7)
@@ -33,3 +44,15 @@ console.log(resultA7)
 let a10='кг'
 let resultA10=`75 ${a10}`
 console.log(resultA10)
+
+// 4 задание
+let height=23
+let width=10
+let result41=height*width
+console.log(result41)
+
+// 5 задание
+let heightC=10
+let dC=4
+let v=3.14*10*4
+console.log(v)
